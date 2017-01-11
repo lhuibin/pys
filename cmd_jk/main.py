9821a8479a7ff84430411c9e8b5f49c8aa3634b8
unknown <>
2017-01-12 06:16:33 +0800

    wakuangjiankong

--- cmd_jk/main.py
+++ cmd_jk/main.py
@@ -4,12 +4,25 @@
 '''
 这是一个监控矿机运行状态的脚本，检测到算力异常自动重启矿机
 '''
+#1.要重启挖矿
+#2.要启动新窗口，也就是执行后，监控程序要继续执行（或者可以重新启动一个监控程序）
 
 import requests
 import re
+import os
+import time
 
-url = 'https://www.f2pool.com/zec/t1b4jWWao8aGzjEd5PCkUCs54PsckRCjLbN'
-r = requests.get(url)
-sol_raw = re.findall('[>]'+'\d{1,3}'+'\s'+'[sol]', r.text)
-sol = re.findall('\d{1,3}',str(sol_raw))
-print('15min:',sol[2])
+while True:
+	time.sleep(600)
+	url = 'https://www.f2pool.com/zec/t1b4jWWao8aGzjEd5PCkUCs54PsckRCjLbN'
+	r = requests.get(url)
+	sol_raw = re.findall('[>]'+'\d{1,3}'+'\s'+'[sol]', r.text)
+	sol = re.findall('\d{1,3}',str(sol_raw))
+	sol15 = int(sol[2])
+	print('15min:',sol[2])
+
+	if sol15 < 100:
+		a=r"f2pool.bat"
+		a= os.path.sep.join(a.split(r'/'))
+		print a
+		os.system(a)
