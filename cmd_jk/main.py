2482721c88e8585a355bb3316a76e4482f27a2e3
unknown <>
2017-01-15 00:40:24 +0800

    添加错误记录日志

--- cmd_jk/main.py
+++ cmd_jk/main.py
@@ -22,7 +22,7 @@ while True:
 		sol = re.findall('\d{1,3}',str(sol_raw))
 		sol15 = int(sol[2])
 		print('15min:',sol[2])
-		#print(a)
+		print a
 
 
 		if sol15 < 1000:
@@ -34,7 +34,7 @@ while True:
 		
 	except Exception as e:
 		logging.exception(e)
-		logger = logging.getLogger("挖矿监控")
+		logger = logging.getLogger("wak")
 		logger.setLevel(logging.ERROR)
 		# 建立一个filehandler来把日志记录在文件里，级别为ERROR以上
 		fh = logging.FileHandler("wakuang.log")
@@ -43,17 +43,18 @@ while True:
 		ch = logging.StreamHandler()
 		ch.setLevel(logging.ERROR)
 		# 设置日志格式
-		formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(e)s")
+		formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 		ch.setFormatter(formatter)
 		fh.setFormatter(formatter)
 		#将相应的handler添加在logger对象中
 		logger.addHandler(ch)
 		logger.addHandler(fh)
 		# 开始打日志
-		logger.debug("debug message")
-		logger.info("info message")
-		logger.warn("warn message")
-		logger.error("error message")
-		logger.critical("critical message")
+		#logger.debug("debug message")
+		#logger.info("info message")
+		#logger.warn("warn message")
+		#logger.error("error message")
+		logger.error(e)
+		#logger.critical("critical message")
 
 	time.sleep(6)
